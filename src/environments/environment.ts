652452c345da651003b6c2e8bacfe96a22be6b36
Rishikakar <67696019+Rishikakar@users.noreply.github.com>
2020-07-12 20:33:17 +0530

    final

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  giphyapi:'qDeWEpSu1sppPQ98a49MblwO6Y9xHSsf'
 };
 
 /*
